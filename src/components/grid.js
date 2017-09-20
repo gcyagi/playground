00613f1c53abfa3eedc2571193e5b7b4e84f52bc
gcyagi <s.yagi@gemcook.com>
2017-09-20 21:44:28 +0900

    build.

--- src/components/grid.js
+++ src/components/grid.js
@@ -5,57 +5,65 @@ class testGrid extends React.Component {
   render() {
     return (
       <div className="testGrid">
-        <div className="pure-u-1-24 defaultColor gridItem">　1-24</div>
-        <div className="pure-u-1-24 defaultColor gridItem">　1-24</div>
-        <div className="pure-u-1-24 defaultColor gridItem">　1-24</div>
-        <div className="pure-u-1-24 defaultColor gridItem">　1-24</div>
-        <div className="pure-u-1-24 defaultColor gridItem">　1-24</div>
-        <div className="pure-u-1-24 defaultColor gridItem">　1-24</div>
-        <div className="pure-u-1-12 defaultColor gridItem">　1-12</div>
-        <div className="pure-u-1-12 defaultColor gridItem">　1-12</div>
-        <div className="pure-u-1-12 defaultColor gridItem">　1-12</div>
-        <div className="pure-u-1-12 defaultColor gridItem">　1-12</div>
-        <div className="pure-u-1-6 defaultColor gridItem">　1-6</div>
-        <div className="pure-u-1-6 defaultColor gridItem">　1-6</div>
-        <div className="pure-u-1-12 defaultColor gridItem">　1-12</div>
+        <div className="pure-u-1-24 defaultColor gridItem">1-24</div>
+        <div className="pure-u-1-24 defaultColor gridItem">1-24</div>
+        <div className="pure-u-1-24 defaultColor gridItem">1-24</div>
+        <div className="pure-u-1-24 defaultColor gridItem">1-24</div>
+        <div className="pure-u-1-24 defaultColor gridItem">1-24</div>
+        <div className="pure-u-1-24 defaultColor gridItem">1-24</div>
+        <div className="pure-u-1-12 defaultColor gridItem">1-12</div>
+        <div className="pure-u-1-12 defaultColor gridItem">1-12</div>
+        <div className="pure-u-1-12 defaultColor gridItem">1-12</div>
+        <div className="pure-u-1-12 defaultColor gridItem">1-12</div>
+        <div className="pure-u-1-6 defaultColor gridItem">1-6</div>
+        <div className="pure-u-1-6 defaultColor gridItem">1-6</div>
+        <div className="pure-u-1-12 defaultColor gridItem">1-12</div>
         <div className="horizon">
-          <div className="pure-u-1-3 horizonOne gridItem">　#08708A</div>
-          <div className="pure-u-1-3 horizonTwo gridItem">　#56B1BF</div>
-          <div className="pure-u-1-3 horizonThree gridItem">　#D0D3C5</div>
+          <div className="pure-u-1-3 horizon_1 gridItem">#08708A</div>
+          <div className="pure-u-1-3 horizon_2 gridItem">#56B1BF</div>
+          <div className="pure-u-1-3 horizon_3 gridItem">#D0D3C5</div>
         </div>
-        <div className="colorPalette redTone pure-u-3-4">
-          <img className="sampleImage pure-u-1-3" src={require("../img/momiji.JPG")} alt="momiji" />
-          <div className="colors pure-u-2-3">
-            <div className="myColorRed_0 gridItem paletteItem">　#BB2F1E</div>
-            <div className="myColorRed_1 gridItem paletteItem">　#E3CB5E</div>
-            <div className="myColorRed_2 gridItem paletteItem">　#C8C781</div>
-            <div className="myColorRed_3 gridItem paletteItem">　#562B15</div>
+        <div className="pure-g">
+          <div className="colorPalette greenTone pure-u-1-2">
+            <img className="sampleImage pure-u-2-3" src={require("../img/radybug.JPG")} alt="spring" />
+            <div className="colors pure-u-1-3">
+              <div className="myColorGreen_0 gridItem paletteItem">#385918</div>
+              <div className="myColorGreen_1 gridItem paletteItem">#9DAD53</div>
+              <div className="myColorGreen_2 gridItem paletteItem">#A1B258</div>
+              <div className="myColorGreen_3 gridItem paletteItem">#E5F3CF</div>
+            </div>
           </div>
-        </div>
-        <div className="colorPalette blueTone">
-          <div className="pure-u-1-4 myColorBlue_0 gridItem">　#003066</div>
-          <div className="pure-u-1-4 myColorBlue_1 gridItem">　#024096</div>
-          <div className="pure-u-1-4 myColorBlue_2 gridItem">　#06C2F4</div>
-          <div className="pure-u-1-4 myColorBlue_3 gridItem">　#97EAF4</div>
-        </div>
-        <div className="colorPalette greenTone">
-          <div className="pure-u-1-4 myColorGreen_0 gridItem">　#404616</div>
-          <div className="pure-u-1-4 myColorGreen_1 gridItem">　#668D3C</div>
-          <div className="pure-u-1-4 myColorGreen_2 gridItem">　#B1DDA1</div>
-          <div className="pure-u-1-4 myColorGreen_3 gridItem">　#E5F3CF</div>
-        </div>
-        <div className="colorPalette yellowTone">
-          <div className="pure-u-1-4 myColorYellow_0 gridItem">　#FFAC00</div>
-          <div className="pure-u-1-4 myColorYellow_1 gridItem">　#FFD319</div>
-          <div className="pure-u-1-4 myColorYellow_2 gridItem">　#FFFF00</div>
-          <div className="pure-u-1-4 myColorYellow_3 gridItem">　#FFFF55</div>
-        </div>
-        <div className="colorPalette gradation">
-          <div className="pure-u-1-5 myColorYellow_0 gridItem">　#FFAC00</div>
-          <div className="pure-u-1-5 myColorYellow_1 gridItem">　#FFD319</div>
-          <div className="pure-u-1-5 myColorYellow_3 gridItem">　#FFFF55</div>
-          <div className="pure-u-1-5 myColorYellow_2 gridItem">　#FFFF00</div>
-          <div className="pure-u-1-5 myColorYellow_1 gridItem">　#FFD319</div>
+
+          <div className="colorPalette yellowTone pure-u-1-2">
+            <img className="sampleImage pure-u-2-3" src={require("../img/sunflower.JPG")} alt="summer" />
+            <div className="colors pure-u-1-3">
+              <div className="myColorYellow_0 gridItem paletteItem">#AA6724</div>
+              <div className="myColorYellow_1 gridItem paletteItem">#F2C242</div>
+              <div className="myColorYellow_2 gridItem paletteItem">#F6F6F6</div>
+              <div className="myColorYellow_3 gridItem paletteItem">#8FA845</div>
+            </div>
+          </div>
+
+          <div className="colorPalette redTone pure-u-1-2">
+            <img className="sampleImage pure-u-2-3" src={require("../img/momiji.JPG")} alt="autumn" />
+            <div className="colors pure-u-1-3">
+              <div className="myColorRed_0 gridItem paletteItem">#BB2F1E</div>
+              <div className="myColorRed_1 gridItem paletteItem">#E3CB5E</div>
+              <div className="myColorRed_2 gridItem paletteItem">#C8C781</div>
+              <div className="myColorRed_3 gridItem paletteItem">#562B15</div>
+            </div>
+          </div>
+
+          <div className="colorPalette blueTone pure-u-1-2">
+            <img className="sampleImage pure-u-2-3" src={require("../img/snow.JPG")} alt="winter" />
+            <div className="colors pure-u-1-3">
+              <div className="myColorBlue_0 gridItem paletteItem">#28437B</div>
+              <div className="myColorBlue_1 gridItem paletteItem">#4578D6</div>
+              <div className="myColorBlue_2 gridItem paletteItem">#6B8CCE</div>
+              <div className="myColorBlue_3 gridItem paletteItem">#92A8DB</div>
+            </div>
+          </div>
+
         </div>
       </div>
     )
